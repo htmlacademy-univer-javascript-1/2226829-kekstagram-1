85b9ddffeb798af9d01c9316821fc1acaacf3746
MonkeyProger <torndmorgan@gmail.com>
2022-12-15 15:38:59 +0300

    11part ready2go

--- js/photo-upload.js
+++ js/photo-upload.js
@@ -2,15 +2,20 @@ import {isKeyEsc} from './util.js';
 import {renderSlider,resetEffect} from './photo-effect.js';
 import {renderScale,resetScale} from './scale.js';
 import {pristine} from './validate.js';
+import {sendData} from './api.js';
+
+const form = document.querySelector('#upload-select-image');
 
 const fileUploadButton = document.querySelector('#upload-file');
 const overlay = document.querySelector('.img-upload__overlay');
-
 const photoUploadForm = document.querySelector('.img-upload__form');
 const textHashtags = photoUploadForm.querySelector('.text__hashtags');
 const textDescription = photoUploadForm.querySelector('.text__description');
-
 const buttonCancelElement = photoUploadForm.querySelector('.img-upload__cancel');
+
+const errorTemplate = document.querySelector('#error');
+const successTemplate = document.querySelector('#success');
+const submitFormElement = form.querySelector('.img-upload__submit');
 
 const closeOverlay = () => {
   resetEffect();
@@ -38,12 +43,52 @@ const uploadFile = () => {
   });
 };
 
+const createSuccessBlock = () => {
+  const successCopy = successTemplate.cloneNode(true).content.querySelector('.success');
+
+  successCopy.addEventListener(
+    'click',
+    (evt) => {
+      if (evt.target.className !== 'success__inner' && evt.target.className !== 'success__title') {
+        document.body.removeChild(successCopy);
+        closeOverlay();
+      }
+    });
+  document.body.appendChild(successCopy);
+};
+
+const createErrorBlock = (text) => {
+  const errorCopy = errorTemplate.cloneNode(true).content.querySelector('.error');
+  errorCopy.querySelector('.error__title').textContent = text;
+
+  errorCopy.addEventListener(
+    'click',
+    (evt) => {
+      if (evt.target.className !== 'error__inner' && evt.target.className !== 'error__title') {
+        document.body.removeChild(errorCopy);
+      }
+    });
+  document.body.appendChild(errorCopy);
+};
+
+
 export const renderFileUpload = () => {
-  photoUploadForm.addEventListener('submit', (evt) => {
-    const isValid = pristine.validate();
-    if (!isValid){
+  photoUploadForm.addEventListener('submit',
+    (evt) => {
       evt.preventDefault();
-    }
-  });
+      const isValid = pristine.validate();
+      if (isValid) {
+        submitFormElement.textContent = 'Опубликовать';
+        sendData(
+          createErrorBlock,
+          createSuccessBlock,
+          new FormData(form));
+        closeOverlay();
+      } else {
+        submitFormElement.textContent = 'Проверьте введенные данные...';
+      }
+    });
   uploadFile();
+  resetEffect();
+  resetScale();
 };
